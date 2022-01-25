56f5d2414d2fb9af16ff30a6998d9577d7bf34dc
Prince Pandey <princepandey2784@gmail.com>
2022-01-25 21:05:42 +0530

    api linked

--- app/src/main/java/com/example/headlinesofday/MainActivity.kt
+++ app/src/main/java/com/example/headlinesofday/MainActivity.kt
@@ -4,31 +4,58 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
+    private lateinit var  mAdepter:NewsListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //        layout manger
         recycleView.layoutManager=LinearLayoutManager(this)
-        val items=fetchData()
-        val adapter= NewsListAdapter(items,this)
-        recycleView.adapter=adapter
+        fetchData()
+        mAdepter= NewsListAdapter(this)
+        recycleView.adapter=mAdepter
 
     }
-    private fun fetchData():ArrayList<String>{
-        val list=ArrayList<String>()
-        for(i in 0 until 100){
-            list.add("item $i")
-        }
-        return list
+    private fun fetchData(){
+        val url="https://newsapi.org/v2/everything?q=apple&from=2022-01-24&to=2022-01-24&sortBy=popularity&apiKey=094b9d78b3364f87b80edecb3c81df5e"
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET,
+            url,
+            null,
+            Response.Listener {
+                val newsJsonArray=it.getJSONArray("articles")
+                val newsArray=ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject=newsJsonArray.getJSONObject(i)
+                    val news=News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+                mAdepter.updateNews(newsArray)
+            },
+            Response.ErrorListener {
+
+            }
+        )
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this,"clicked item is $item",Toast.LENGTH_SHORT).show()
+    override fun onItemClicked(item: News) {
+
     }
 
 
 }
+
+
--- app/src/main/java/com/example/headlinesofday/NewsListAdapter.kt
+++ app/src/main/java/com/example/headlinesofday/NewsListAdapter.kt
@@ -6,7 +6,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NewsListAdapter(private val items:ArrayList<String>,private val listener:NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+class NewsListAdapter(private val listener:NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+    private val items:ArrayList<News> = ArrayList()
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
@@ -23,8 +24,16 @@ class NewsListAdapter(private val items:ArrayList<String>,private val listener:N
     }
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         //binder
+
         val currentItem=items[position]
-        holder.titleView.text=currentItem
+        holder.titleView.text=currentItem.title
+    }
+    fun updateNews(updatedNews:ArrayList<News>)
+    {
+        items.clear()
+        items.addAll(updatedNews)
+
+        notifyDataSetChanged()
     }
 }
 class NewsViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
@@ -32,5 +41,6 @@ class NewsViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
 
 }
 interface NewsItemClicked{
-    fun onItemClicked(item:String)
+    fun onItemClicked(item:News)
+
 }
