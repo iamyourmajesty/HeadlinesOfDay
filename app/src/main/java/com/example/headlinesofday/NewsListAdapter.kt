e44b59073327588a2362a61907b0123d42cc42db
Prince Pandey <princepandey2784@gmail.com>
2022-01-25 21:54:54 +0530

    Image and author added

--- app/src/main/java/com/example/headlinesofday/NewsListAdapter.kt
+++ app/src/main/java/com/example/headlinesofday/NewsListAdapter.kt
@@ -3,8 +3,10 @@ package com.example.headlinesofday
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class NewsListAdapter(private val listener:NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
     private val items:ArrayList<News> = ArrayList()
@@ -27,6 +29,9 @@ class NewsListAdapter(private val listener:NewsItemClicked): RecyclerView.Adapte
 
         val currentItem=items[position]
         holder.titleView.text=currentItem.title
+        holder.author.text=currentItem.author
+        //Image is handeled or binded by Glide
+        Glide.with(holder.itemView.context).load(currentItem.imageUrl).into(holder.image)
     }
     fun updateNews(updatedNews:ArrayList<News>)
     {
@@ -38,6 +43,8 @@ class NewsListAdapter(private val listener:NewsItemClicked): RecyclerView.Adapte
 }
 class NewsViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
     val titleView:TextView =itemView.findViewById(R.id.title)
+    val image:ImageView=itemView.findViewById(R.id.image)
+    val author:TextView=itemView.findViewById(R.id.author)
 
 }
 interface NewsItemClicked{
